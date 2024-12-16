11d09bd4bd77cf142ec0b86b46a0406b86117225
vldnvs <127663803+vldnvs@users.noreply.github.com>
2024-12-16 17:54:45 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -15,20 +15,19 @@
 // })();
 
 function setLoadTime(){
+    const timing = performance.timing;
+    const loadTime = timing.loadEventEnd - timing.navigationStart;
+    const footer = document.querySelector("footer");
+    const statsElement = document.createElement("p");
 
-    const [navigation] = performance.getEntriesByType("navigation");
-
-    if (navigation) {
-        const loadTime = navigation.loadEventEnd - navigation.startTime;
-        const footer = document.querySelector("footer");
-        const statsElement = document.createElement("p");
-
+    if (loadTime > 0) {
         statsElement.textContent = `Page load time is ${(loadTime / 1000).toFixed(2)} seconds`;
-        footer.appendChild(statsElement);
+    } else {
+        statsElement.textContent = "Page load time could not be measured.";
     }
+
+    footer.appendChild(statsElement);
 }
 
-window.addEventListener("load", () => {
-    setLoadTime()
-});
+window.addEventListener("load", () => setTimeout(setLoadTime, 0));
 
